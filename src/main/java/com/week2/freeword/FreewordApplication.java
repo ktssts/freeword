1e36953bec9c8ed8c0448d83f024abdb3718d97a
ktssts <ub.songwenxu@gmail.com>
2021-11-16 16:26:59 +0900

    Initial commit

---
+++ src/main/java/com/week2/freeword/FreewordApplication.java
@@ -0,0 +1,13 @@
+package com.week2.freeword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreewordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreewordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/week2/freeword/FreewordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.week2.freeword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreewordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
