119c405de7bca604b491f38cbba79d89e796534c
ktssts <ub.songwenxu@gmail.com>
2021-11-18 16:26:42 +0900

    Final

--- src/main/java/com/week3/freeword/FreewordApplication.java
+++ src/main/java/com/week3/freeword/FreewordApplication.java
@@ -2,12 +2,21 @@ package com.week3.freeword;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class FreewordApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FreewordApplication.class, args);
 	}
+	@PostConstruct
+	public void started(){
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 
+	}
 }
